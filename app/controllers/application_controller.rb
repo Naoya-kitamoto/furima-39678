aaaedb7e48a96f2636f0154878a5b1f029ee83a5
Naoya-kitamoto <k_naoya_1028@ezweb.ne.jp>
2023-08-03 17:46:14 +0900

    ユーザー認証機能の導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == 'Naoya-kita' && password == 'kn.102833'
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
   end
 end
