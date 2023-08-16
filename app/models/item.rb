3420e24f32f5964b809239f957ac30066424e5ee
Naoya-kitamoto <k_naoya_1028@ezweb.ne.jp>
2023-08-16 21:10:46 +0900

    ビューファイルの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,12 @@
 class Item < ApplicationRecord
+  validates :item_name,         presence: true
+  validates :explanation,         presence: true
+  validates :category_id,         presence: true
+  validates :sender_id,         presence: true
+  validates :delivery_charge_id,         presence: true
+  validates :delivery_day_id,         presence: true
+  validates :price,         presence: true
+
+  belongs_to :user
+  has_one :purchase
 end
