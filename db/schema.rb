f47dd1f1c7bc25dc434f477b650f3d45e745144d
Naoya-kitamoto <k_naoya_1028@ezweb.ne.jp>
2023-08-16 19:48:20 +0900

    マイグレーションファイルの作成とバリデーションの設定

--- db/schema.rb
+++ db/schema.rb
@@ -12,16 +12,14 @@
 
 ActiveRecord::Schema[7.0].define(version: 2023_08_11_030331) do
   create_table "users", charset: "utf8", force: :cascade do |t|
-    t.string "nickname", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "last_name", default: "", null: false
-    t.string "first_name", default: "", null: false
-    t.string "last_name_kana", default: "", null: false
-    t.string "first_name_kana", default: "", null: false
-    t.string "burth_year", default: "", null: false
-    t.string "burth_mounth", default: "", null: false
-    t.string "burth_day", default: "", null: false
+    t.string "nickname", null: false
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.string "first_name_kana", null: false
+    t.string "last_name_kana", null: false
+    t.date "birth_day", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
