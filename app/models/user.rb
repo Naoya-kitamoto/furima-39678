db6934a8f23db9962eee526d2916ad026731eaef
Naoya-kitamoto <k_naoya_1028@ezweb.ne.jp>
2023-08-21 14:20:00 +0900

    カタカナの制限とエラーメッセージの表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,10 @@ class User < ApplicationRecord
     validates :nickname,         presence: true
     validates :last_name,        presence: true
     validates :first_name,       presence: true
-    validates :last_name_kana,   presence: true
-    validates :first_name_kana,  presence: true
+    validates :last_name_kana,   presence: true,
+    format: { with: /\A[\p{katakana}ー－]+\z/ }
+    validates :first_name_kana,  presence: true,
+    format: { with: /\A[\p{katakana}ー－]+\z/ }
     validates :birth_day,        presence: true
 
     has_many :items
