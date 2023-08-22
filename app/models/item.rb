9a15783b4ee0b9cbc24a017bf52a76edbe0619bd
Naoya-kitamoto <k_naoya_1028@ezweb.ne.jp>
2023-08-22 16:47:52 +0900

    アクティブハッシュの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,21 @@
 class Item < ApplicationRecord
   validates :item_name,           presence: true
   validates :explanation,         presence: true
-  validates :category_id,         presence: true
-  validates :sender_id,           presence: true
-  validates :delivery_charge_id,  presence: true
-  validates :delivery_day_id,     presence: true
+  validates :category_id,         presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :condition_id         presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :sender_id,           presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :delivery_charge_id,  presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :delivery_day_id,     presence: true, numericality: { other_than: 1 , message: "can't be blank"}
   validates :price,               presence: true
   validates :image,               presence: true
 
   belongs_to :user
   #has_one :purchase
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery_charge
+  belongs_to :delivery_day
+  belongs_to :sender
 end
