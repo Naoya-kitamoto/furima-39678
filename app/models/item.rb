571252a65b4265dd6df8041389f563d55fb09aef
Naoya-kitamoto <k_naoya_1028@ezweb.ne.jp>
2023-08-22 15:25:40 +0900

    バリデーションの追加とアクティブハッシュ実装の準備

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,13 +1,14 @@
 class Item < ApplicationRecord
-  #validates :item_name,         presence: true
-  #validates :explanation,         presence: true
-  #validates :category_id,         presence: true
-  #validates :sender_id,         presence: true
-  #validates :delivery_charge_id,         presence: true
-  #validates :delivery_day_id,         presence: true
-  #validates :price,         presence: true
+  validates :item_name,           presence: true
+  validates :explanation,         presence: true
+  validates :category_id,         presence: true
+  validates :sender_id,           presence: true
+  validates :delivery_charge_id,  presence: true
+  validates :delivery_day_id,     presence: true
+  validates :price,               presence: true
+  validates :image,               presence: true
 
-  #belongs_to :user
+  belongs_to :user
   #has_one :purchase
   has_one_attached :image
 end
