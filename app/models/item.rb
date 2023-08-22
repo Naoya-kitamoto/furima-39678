40ba665d2725fd0a2bf001b99998cc1d86cf8815
Naoya-kitamoto <k_naoya_1028@ezweb.ne.jp>
2023-08-22 17:18:41 +0900

    ミスの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,7 @@ class Item < ApplicationRecord
   validates :item_name,           presence: true
   validates :explanation,         presence: true
   validates :category_id,         presence: true, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :condition_id         presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :condition_id,         presence: true, numericality: { other_than: 1 , message: "can't be blank"}
   validates :sender_id,           presence: true, numericality: { other_than: 1 , message: "can't be blank"}
   validates :delivery_charge_id,  presence: true, numericality: { other_than: 1 , message: "can't be blank"}
   validates :delivery_day_id,     presence: true, numericality: { other_than: 1 , message: "can't be blank"}
