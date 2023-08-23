b40b7560a7cb51b1d7dd75883c6d45eb683dfa13
Naoya-kitamoto <k_naoya_1028@ezweb.ne.jp>
2023-08-23 10:35:06 +0900

    値段表記のバリデーションの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,7 +6,8 @@ class Item < ApplicationRecord
   validates :sender_id,           presence: true, numericality: { other_than: 1 , message: "can't be blank"}
   validates :delivery_charge_id,  presence: true, numericality: { other_than: 1 , message: "can't be blank"}
   validates :delivery_day_id,     presence: true, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :price,               presence: true
+  validates :price,               presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 },
+                                  format: { with: /\A\d+\z/, message: "半角数字のみを入力してください" }
   validates :image,               presence: true
 
   belongs_to :user
