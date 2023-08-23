374f64cc6e987e51d6bccedf88a6b73db9ea2c30
Naoya-kitamoto <k_naoya_1028@ezweb.ne.jp>
2023-08-23 13:30:00 +0900

    javascript内の記入

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -2,3 +2,17 @@ window.addEventListener('turbo:load', () => {
   console.log("OK");
 });
 
+const priceInput = document.getElementById("item-price");
+const addTaxDom = document.getElementById("add-tax-price");
+const addProfitDom = document.getElementById("profit")
+
+priceInput.addEventListener("input", () => {
+  const inputValue = priceInput.value;
+  if (inputValue !== "") {
+    const price = parseInt(inputValue, 10);
+    const tax = Math.floor(price * 0.1);
+    const profit = price - tax;
+    addTaxDom.innerHTML = `${tax}`;
+    addProfitDom.innerHTML = `${profit}`;
+  }
+});
