bc0cbcbc016a68cfbbb7fac1d8c666f6dad0ccd1
Naoya-kitamoto <k_naoya_1028@ezweb.ne.jp>
2023-08-23 16:12:31 +0900

    テストコードの入力

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,7 +10,7 @@ class Item < ApplicationRecord
                                   format: { with: /\A\d+\z/, message: "only use half width number" }
   validates :image,               presence: true
 
-  belongs_to :user
+  #belongs_to :user
   #has_one :purchase
   has_one_attached :image
   extend ActiveHash::Associations::ActiveRecordExtensions
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -16,7 +16,7 @@ RSpec.describe Item, type: :model do
       it "item_nameが空だと出品できない" do
         @item.item_name = ''
         @item.valid?
-        expect(@item.errors.full_messages).to include("Item Name can't be blank")
+        expect(@item.errors.full_messages).to include("Item name can't be blank")
       end
       it "explanationが空だと出品できない" do
         @item.explanation = ''
@@ -26,27 +26,27 @@ RSpec.describe Item, type: :model do
       it "category_idが空では出品できない" do
         @item.category_id = ''
         @item.valid?
-        expect(@item.errors.full_messages).to include("Category Id can't be blank")
+        expect(@item.errors.full_messages).to include("Category can't be blank")
       end
       it "condition_idが空では出品できない" do
         @item.condition_id = ''
         @item.valid?
-        expect(@item.errors.full_messages).to include("Condition Id can't be blank")
+        expect(@item.errors.full_messages).to include("Condition can't be blank")
       end
       it "sender_idが空だと出品できない" do
         @item.sender_id = ''
         @item.valid?
-        expect(@item.errors.full_messages).to include("Sender Id can't be blank")
+        expect(@item.errors.full_messages).to include("Sender can't be blank")
       end
       it "delivery_charge_idが空では出品できない" do
         @item.delivery_charge_id = ''
         @item.valid?
-        expect(@item.errors.full_messages).to include("Delivery Charge Id can't be blank")
+        expect(@item.errors.full_messages).to include("Delivery charge can't be blank","Delivery charge can't be blank")
       end
       it "delivery_day_idが空だと出品できない" do
         @item.delivery_day_id = ''
         @item.valid?
-        expect(@item.errors.full_messages).to include("Delivery Day Id can't be blank")
+        expect(@item.errors.full_messages).to include("Delivery day can't be blank", "Delivery day can't be blank")
       end
       it "priceが空では出品できない" do
         @item.price = ''
@@ -54,34 +54,34 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Price can't be blank")
       end
       it "imageが空だと出品できない" do
-        @item.image = ''
+        @item.image = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Image can't be blank")
       end
       it "category_idが1だと出品できない" do
         @item.category_id = '1'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Category Id can't be blank")
+        expect(@item.errors.full_messages).to include("Category can't be blank")
       end
       it "condition_idが1だと出品できない" do
         @item.condition_id = '1'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Condition Id can't be blank")
+        expect(@item.errors.full_messages).to include("Condition can't be blank")
       end
       it "sender_idが1だと出品できない" do
         @item.sender_id = '1'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Sender Id can't be blank")
+        expect(@item.errors.full_messages).to include("Sender can't be blank")
       end
       it "delivery_charge_idが1だと出品できない" do
-        @item.delivery_char ge_id = '1'
+        @item.delivery_charge_id = '1'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Delivery Charge Id can't be blank")
+        expect(@item.errors.full_messages).to include("Delivery charge can't be blank")
       end
       it "delivery_day_idが1だと出品できない" do
-        @item.condition_id = '1'
+        @item.delivery_day_id = '1'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Delivery Day Id can't be blank")
+        expect(@item.errors.full_messages).to include("Delivery day can't be blank")
       end
       it "priceが299円以下の場合出品できない" do
         @item.price = '200'
@@ -96,7 +96,7 @@ RSpec.describe Item, type: :model do
       it "priceに半角数字以外が入力された場合出品できない" do
         @item.price = "こんにちは"
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price only use half width number")
+        expect(@item.errors.full_messages).to include("Price is enter between 299 and 1000000")
       end
     end
   end
