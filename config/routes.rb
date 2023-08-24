8ec642ed54ef64138f389352831b526137610edb
Naoya-kitamoto <k_naoya_1028@ezweb.ne.jp>
2023-08-24 18:43:10 +0900

    ルーティングの統一

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,4 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root to: "items#index"
-  resources :items, only: [:index, :create, :new, :show, :edit, :update]
-end
+  resources :items
