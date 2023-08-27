e210fb9df77b4d5b47539ce1ed819cfc7f75f40b
Naoya-kitamoto <k_naoya_1028@ezweb.ne.jp>
2023-08-27 17:13:55 +0900

    purchacesモデルの作成

--- db/migrate/20230827073046_create_addresses.rb
+++ db/migrate/20230827073046_create_addresses.rb
@@ -1,14 +1,13 @@
 class CreateAddresses < ActiveRecord::Migration[7.0]
   def change
     create_table :addresses do |t|
-t.string :post_code               null: false
-t.integer :prefecture_id          null: false
-t.string :municipalities          null: false
-t.string :street_address          null: false
-t.string :building_name
-t.string :phone_number            null: false
-t.references :purchase           :references foreign_key: true null: false
-
+      t.string :post_code,               null: false
+      t.integer :prefecture_id,          null: false
+      t.string :municipalities,          null: false
+      t.string :street_address,          null: false
+      t.string :building_name
+      t.string :phone_number,            null: false
+      t.references :purchase,            foreign_key: true, null: false
       t.timestamps
     end
   end
