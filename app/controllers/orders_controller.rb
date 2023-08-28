79eea1ad2079fceaa119168933e72960cc62d20c
Naoya-kitamoto <k_naoya_1028@ezweb.ne.jp>
2023-08-28 16:30:12 +0900

    コントローラーの定義

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,21 @@
 class OrdersController < ApplicationController
+  def index
+    @purchase_address = PurchaseAddress.new
+    @item = Item.find(params[:item_id])
+  end
+  def create
+    @purchase_address = PurchaseAddress.new(parchase_params)
+    if @purchase_address.valid?
+      @purchase_address.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def purchase_params
+    params.require(:purchase_address).permit(:post_code, :sender_id, :municipalities, :street_address, :building_name, :phone_number).merge(user_id: current_user.id)
+  end
 end
--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -13,10 +13,10 @@ class PurchaseAddress
     validates :item_id
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
   end
-  validates :prefecture, numericality: {other_than: 0, message: "can't be blank"}
+  validates :sender_id, numericality: {other_than: 0, message: "can't be blank"}
 
   def save
     purchase = Purchase.create(item_id: item_id, user_id: user_id)
-    Address.create(post_code: post_code, prefecture: prefecture, municipalities: municipalities, street_address: street_address, building_name: building_name, phone_number: phone_number, purchase_id: purchase.id)
+    Address.create(post_code: post_code, sender_id: sender_id, municipalities: municipalities, street_address: street_address, building_name: building_name, phone_number: phone_number, purchase_id: purchase.id)
   end
 end
