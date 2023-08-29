bb59b83d10f778e4c0d0d8453f7009abc2520800
Naoya-kitamoto <k_naoya_1028@ezweb.ne.jp>
2023-08-29 21:19:19 +0900

    テストコードの記述

---
+++ spec/factories/purchase_addresses.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :purchase_address do
+    municipalities{"川崎市"}
+    street_address{"5-5-5"}
+    phone_number{"09009000900"}
+    post_code{"123-4567"}
+    sender_id{14}
+    token {"tok_abcdefghijk00000000000000000"}
+  end
+end
