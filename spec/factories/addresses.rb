512d34995c7922469564281ed536a6eec3076a3e
Naoya-kitamoto <k_naoya_1028@ezweb.ne.jp>
2023-08-29 11:54:29 +0900

    テストコードの訂正

--- spec/factories/addresses.rb
+++ spec/factories/addresses.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :address do
-    association :purchase_address
+    association :purchase, factory: :purchase
     municipalities{"川崎市"}
     street_address{"5-5-5"}
-    phona_number{"09009000900"}
+    phone_number{"09009000900"}
     post_code{"123-4567"}
     sender_id{14}
   end
