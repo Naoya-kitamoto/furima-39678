ab702cf60f4630ec1f15ea3bf7250f9ab242f037
Naoya-kitamoto <k_naoya_1028@ezweb.ne.jp>
2023-08-29 11:36:30 +0900

    テストコードの追加

--- spec/factories/addresses.rb
+++ spec/factories/addresses.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :address do
-    
+    association :purchase_address
+    municipalities{"川崎市"}
+    street_address{"5-5-5"}
+    phona_number{"09009000900"}
+    post_code{"123-4567"}
+    sender_id{14}
   end
 end
